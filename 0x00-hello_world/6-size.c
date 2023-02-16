0a1f3f24eb6e645e662ff3f3a58f806e74b67759
Brook <broook@skiff.com>
2023-02-16 14:42:09 -0800

    updated 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,7 @@ int main(void)
 	printf("Size of an int: %ld byte(s)\n", sizeof(integerType));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(longintegerType));
 	printf("Size of a long long int: %1d byte(s)\n", sizeof(longlongintegerType));
-	printf("Size of a float: %1d bytes\n", sizeof(floatType));
+	printf("Size of a float: %1d byte(s)\n", sizeof(floatType));
 
 return (0);
 }
