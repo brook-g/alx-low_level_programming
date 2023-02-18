9ca7b2a25d6e94d6c8b3c891b387799a8ee0bd06
Brook <broook@skiff.com>
2023-02-17 16:03:26 -0800

    update

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,15 +13,14 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	x = n % 10;	
-	printf("Last digit of %d is %d ", n, x);
+	printf("Last digit of %d is %d ", n, n % 10);
 	if (n % 10 > 5)
 	{
 		printf("and is greater than 5\n");
 	}
 	else if (n % 10 == 0)
 	{
-		printf("0\n");
+		printf("0 and is \n");
 	}
 	else
 	{
