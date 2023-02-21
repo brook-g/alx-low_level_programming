fa27c227f8d54c118e8b96eae90d56daeb91588c
Brook <broook@skiff.com>
2023-02-21 12:27:46 -0800

    $CMSG

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,21 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
- * print_alphabet_x10 - prints alphabet in lowercase 10 times
+ * print_alphabet_x10 - Prints the alphabet 10 times.
  *
  * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
 {
-	for (int a = 0 ; a <= 9 ; a++)
+	int a, b;
+
+	for (a = 0; a <= 9; a++)
 	{
-		for (char ch = 'a'; ch <= 'z' ; c++)
+		for (b = 97; b <= 122; b++)
 		{
-			_putchar (ch);
+			_putchar(b);
 		}
-		putchar ('\n');
+	_putchar('\n');
 	}
-	return (0);
 }
