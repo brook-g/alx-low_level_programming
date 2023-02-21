240de610e129cf244a2e5afaeb5145d014b77cfb
Brook <broook@skiff.com>
2023-02-21 08:33:04 -0800

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,20 @@
 #include "main.h"
-
 /**
- * main - Entry point
+ *main - Entry point
  *
- * Return: Always 0 (Success)
+ *Return: nothing, return void
  */
 int main(void)
 {
-	char ch[] = "_putchar";
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	for (int i = 0; i < 8; i++)
-	{
-		putchar(ch[i]);
-	}
-	putchar('\n');
 	return (0);
 }
