4e745461826d83a60a188deb853fdadfb4b3308b
Brook <broook@skiff.com>
2023-02-21 12:03:21 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet in lowercase 10 times
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+
+	for (int c = 0 ; c <= 9 ; c++)
+	{
+		for (c = 'a'; c <= 'z' ; c++)
+		{
+			_putchar (ch);
+		}
+		putchar ('\n');
+	}
+	return (0);
+}
