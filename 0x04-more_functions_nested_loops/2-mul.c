6c81ed186a50c11e747f9bafbf7e6ed9c3e9d31c
Brook <broook@skiff.com>
2023-02-23 08:50:42 -0800

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: product as int
+ */
+int mul(int a, int b)
+{
+	if (int a >= '0' && int a <= '9')
+	{
+		if (int b >= '0' && int a <= '9')
+		{
+			return (a * b);
+		}
+	}
+}
