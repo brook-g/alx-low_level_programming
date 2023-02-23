5c6fa327a3306fe4f168beef21e7dc0026c707e4
Brook <broook@skiff.com>
2023-02-23 09:35:51 -0800

    print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - function that prints numbers from 0 to 9
+ * except numbers 2 and 4 to the standard output
+ *
+ * Return: always 0.
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 48; a < 58 ; a++)
+	{
+		for (a != 50 ; a != 52)
+		{
+			putchar(a);
+		}
+	}
+	putchar(10);
+}
