d6aaee59ed24573c1e4fab01e5a116eb6140991d
Brook <broook@skiff.com>
2023-02-23 07:57:03 -0800

    first commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "_isupper.h"
+#include <main.h>
+/**
+ *_isupper.h - checks for uppercase characters in a string.
+ *
+ *@c - uppercase
+ *
+ *return - 1 if uppercase is found. 0 if not found.
+ * 
+ */ 
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
