ce54362616f6c1246c55cd685e016905dc44264b
Brook <broook@skiff.com>
2023-02-23 13:05:21 -0800

    add

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Debugs the positive_or_negative function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	a = 0;
+	positive_or_negative(a);
+	return (0);
+}
