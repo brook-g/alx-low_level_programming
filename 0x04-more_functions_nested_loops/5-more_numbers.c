1ef4ed6770bbe0d950bed2cc38d6f5102be030fc
Brook <broook@skiff.com>
2023-02-23 10:03:16 -0800

    more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * more_numbers - Prints 10 times the numbers, from 0 to 14
@@ -11,15 +12,14 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-	for (j = 0; j <= 14; j++)
-	{
-		if (j > 9)
+		for (j = 0; j <= 14; j++)
 		{
-			_putchar((j / 10) + '0');
+			if (j > 9)
+			{
+				putchar((j / 10) + '0');
+			}
+			putchar((j % 10) + '0');
 		}
-		_putchar((j % 10) + '0');
-	}
-	_putchar('\n');
+		putchar('\n');
 	}
 }
-
