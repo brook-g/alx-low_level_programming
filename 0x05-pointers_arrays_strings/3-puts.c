76d401420ef0d0941cb7d3bacce56476a4ad34dc
Brook <broook@skiff.com>
2023-02-27 10:42:19 -0800

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to standard output.
+ * @str: input string.
+ * Return - nothing to return.
+ */
+
+void _puts(char *str)
+{
+	putchar(*str);
+	putchar(\n);
+}
