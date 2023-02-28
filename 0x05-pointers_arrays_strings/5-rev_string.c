63007339fd3f024b0d34f6b3a4ece5894387a87a
Brook <broook@skiff.com>
2023-02-28 00:46:22 -0800

    print rev

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,11 +8,12 @@
 void print_rev(char *s)
 {
 	int len = 0;
+
 	while (s[len] != '\0')
 	{
 		len++;
 	}
-	for (int i = len -1; i >= 0; i--)
+	for (int i = len - 1; i >= 0; i--)
 	{
 		putchar(s[i]);
 	}
