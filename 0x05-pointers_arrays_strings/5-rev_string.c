a9a446baff100ec5773a746666ac3055c37ba4e4
Brook <broook@skiff.com>
2023-02-28 01:11:49 -0800

    print rev

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_rev - prints string in reverse.
@@ -18,7 +17,7 @@ void print_rev(char *s)
 			break;
 		count++;
 	}
-	*str = s;
+	str = s;
 
 	for (i = 0; i < (count - 1); i++)
 	{
