440feedbb648731b87cb92ee51bf25415ce2d76a
Brook <broook@skiff.com>
2023-03-06 04:59:22 -0800

    update

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memcpy(char *dest, char *src, unsigned int n): copies data
+ * from source src to destination dest.
+ * @src: source data.
+ * @dest: destination memory area.
+ * @n: bytes.
+ * Return: *dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
+}
