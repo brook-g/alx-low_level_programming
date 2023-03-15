47b537728617e6069c826aa7438534ec33fff52d
Brook <broook@skiff.com>
2023-03-15 06:10:26 -0700

    char *_strdup(char *str)

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,18 +11,17 @@
 
 char *_strdup(char *str)
 {
-	int i;
 	unsigned int size = strlen(str) + 1;
 
 	if (str == NULL)
-		return (NULL);
+		return NULL;
 
 	char *str2 = (char *) malloc(size * sizeof(char));
 
 	if (str2 == NULL)
-		return (NULL);
+		return NULL;
 
 	strcpy(str2, str);
 
-	return (str2);
+	return str2;
 }
