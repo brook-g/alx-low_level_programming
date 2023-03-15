923f30c39d42c7200e222066a8b4546133b4d10f
Brook <broook@skiff.com>
2023-03-15 12:24:35 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,37 +1,43 @@
-#include <stdlib.h>
-#include <string.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * *str_concat - concatenates two strings
- * @s1: first string
- * @s2: second string
- * Return: result
+ * str_concat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ *
+ * Return: pointer of an array of chars
  */
-
 char *str_concat(char *s1, char *s2)
 {
-	int len1, len2
-	char *result;
+	char *strout;
+	unsigned int i, j, k, limit;
 
-	if (s1 == NUL)
+	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	len1 = strlen(s1);
-	len2 = strlen(s2);
+	for (i = 0; s1[i] != '\0'; i++)
+		;
 
-	*result = (char *) malloc((len1 + len2 + 1) * sizeof(char));
+	for (j = 0; s2[j] != '\0'; j++)
+		;
 
-	if (result == NULL)
+	strout = malloc(sizeof(char) * (i + j + 1));
+
+	if (strout == NULL)
+	{
+		free(strout);
 		return (NULL);
+	}
 
-	strcpy(result, s1);
+	for (k = 0; k < i; k++)
+		strout[k] = s1[k];
 
-	strcat(result, s2);
+	limit = j;
+	for (j = 0; j <= limit; k++, j++)
+		strout[k] = s2[j];
 
-	result[len1 + len2] = '\0';
-
-	return (result);
+	return (strout);
 }
